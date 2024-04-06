2ccc69b44f142fa21d00b0a3bc60ef82137cf1e2
sakthikumaran-04 <sakthikumaranofficial@gmail.com>
2024-04-06 14:56:34 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    manifest:{
+      icons:[
+        {
+          src:"/apple-touch-icon.png",
+          sizes:"512x512",
+          type:"image/png",
+          purpose:"any maskable"
+        }
+      ]
+    }
+  })],
 })
